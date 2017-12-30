84d158e0eb4da4f23b70a5439b14cf5e0b20f6aa
shiyunjie <shiyunjie_nw@163.com>
2017-12-30 19:45:59 +0800

    feat：添加readme

--- app/test/testUtil.js
+++ app/test/testUtil.js
@@ -1,49 +1,20 @@
 'use strict';
 
-var _regenerator = require('babel-runtime/regenerator');
+/**
+ * Created by shiyunjie on 2017/12/30.
+ */
+var util = require('../index');
 
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
-
-var _index = require('../index');
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var main = function () {
-  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
-    var result;
-    return _regenerator2.default.wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return _index2.default.countPackageRequire('app/test');
-
-          case 2:
-            result = _context.sent;
-
-            //const result = util.countFileRows();
-            console.log('main:', result);
-
-          case 4:
-          case 'end':
-            return _context.stop();
-        }
-      }
-    }, _callee, undefined);
-  }));
-
-  return function main() {
-    return _ref.apply(this, arguments);
-  };
-}(); /**
-      * Created by shiyunjie on 2017/12/30.
-      */
-
+function main() {
+  //const result = await util.countFileRows('app', { dot: false }, ['update.ts']);
+  util.default.countFileRows('app').then(function (res) {
+    console.log(res);
+  });
+  //util.default.countPackageRequire('app').then(
+  //  function (res) {
+  //    console.log(res)
+  //  }
+  //)
+}
 
 main();
